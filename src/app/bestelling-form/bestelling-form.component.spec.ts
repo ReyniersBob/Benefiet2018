2737683ab03f119d13983591c8a4aae1edcb10b5
WalleBEEkeren <kenneth_walschaers@telenet.be>
2018-07-21 21:50:42 +0200

    adding bestelling-form

---
+++ src/app/bestelling-form/bestelling-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BestellingFormComponent } from './bestelling-form.component';
+
+describe('BestellingFormComponent', () => {
+  let component: BestellingFormComponent;
+  let fixture: ComponentFixture<BestellingFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BestellingFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BestellingFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/bestelling-form/bestelling-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bestelling-form',
+  templateUrl: './bestelling-form.component.html',
+  styleUrls: ['./bestelling-form.component.css']
+})
+export class BestellingFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
